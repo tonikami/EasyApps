4de5ba0b98d00c4e92d577613e657b7fcd1dea0e
tonikami <tundetalao@gmail.com>
2017-12-24 11:56:24 +0000

    iOS framework

---
+++ ios/EasyAppIos/EasyAppIos/EasyAppIos.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyAppIos.
+FOUNDATION_EXPORT double EasyAppIosVersionNumber;
+
+//! Project version string for EasyAppIos.
+FOUNDATION_EXPORT const unsigned char EasyAppIosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyAppIos/PublicHeader.h>
+
+
---
+++ ios/EasyAppIos/EasyAppIos/EasyAppRequest.swift
@@ -0,0 +1,34 @@
+
+public class EasyAppRequest {
+    public static var requestDomain = ""
+    
+    public static func login(email: String, password: String, completion: @escaping (_ result: [String:Any]?) -> Void) {
+        let urlString = URL(string: requestDomain + "/auth/login/\(email)/\(password)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
+        makeRequest(urlString: urlString, completion: completion)
+    }
+    
+    public static func register(email: String, password: String, name: String, completion: @escaping (_ result: [String:Any]?) -> Void) {
+        let urlString = URL(string: requestDomain + "/auth/register/\(email)/\(password)/\(name)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)!
+        makeRequest(urlString: urlString, completion: completion)
+    }
+    
+    static func makeRequest(urlString: URL, completion: @escaping (_ result: [String:Any]?) -> Void) {
+        let urlRequest = URLRequest(url: urlString)
+        print("sending request")
+        print(urlRequest.description)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if (error == nil && data != nil) {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any]
+                    print("Received valid response")
+                    print(json ?? [])
+                    return completion(json)
+                } catch {
+                }
+            }
+            completion(nil)
+        }
+        task.resume()
+    }
+}
